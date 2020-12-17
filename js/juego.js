9cdb34d030ba1d62c30311f3fea361192962aaad
BryanJoseM79 <bryanjosem79@gmail.com>
2020-12-17 15:09:49 -0300

    Version 1 del juego terminada

--- js/juego.js
+++ js/juego.js
@@ -7,5 +7,33 @@ let MapaDelTesoro = {
     x: ObtenerNumeroAletorio(WIDTH),
     y: ObtenerNumeroAletorio(HEIGH)
 }
-console.log()
+
+
+// Guardamos las variables con  $ para hacer ver que guardamos una variable HTML
+let $mapa = document.getElementById('mapa');
+let $distancia = document.getElementById('distancia');
+
+// creamos el contador de clicks
+let clicks = 0;
+
+
+// Acá escuchamos que cuando el usuario haga click vamos a hacer algo 
+$mapa.addEventListener('click',function (e){
+    clicks++;
+    let distancia = ObtenerDistancia(e,MapaDelTesoro);
+    // Le damos la pista al usuario
+    let DistanciaPista = PistaParaUsuario(distancia);
+
+    // Le enviamos la pista al usuario
+    // Utilizamos las ` ` de JS Para mostrar la variable por pantalla 
+    $distancia.innerHTML = `<h1>${DistanciaPista}</h1>`;
+    
+    // Ahora le daremos la victoria si es que realmente esta cerca del punto 
+
+    if (distancia < 20){
+        alert(`¡Has encontrado el tesoro en ${clicks} clicks!` );
+        // refrescamos la pagina para que se reinicie la pagina luego de ganar 
+        location.reload();
+    }
+})
 
